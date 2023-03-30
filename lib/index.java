71715ee2c483af24cc20cc2389d5344271ab15fc
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 <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>
2023-03-30 23:47:24 +0300

    changes on validity of option

--- lib/index.java
+++ lib/index.java
@@ -20,49 +20,59 @@ public class index {
     }
 
     public static void shapeSwitchLogic() {
-        writeLine("Enter the shape Position:");
         Scanner sc = new Scanner(System.in);
+    
+        // Prompt for position
+        writeLine("Enter the shape Position:");
         writeLine("X: ");
         int x = sc.nextInt();
         writeLine("Y: ");
         int y = sc.nextInt();
         Posn shapePosition = new Posn(x, y);
-
-        writeLine("Please select a shape. The options are: ");
-        writeLine("1. Circle: ");
-        writeLine("2. Square: ");
-        writeLine("3. Rectangle: ");
-        int shapeNumber = sc.nextInt();
-
-        switch (shapeNumber) {
-            case 1:
-                writeLine("Option 1 selected.");
-                writeLine("Please enter the radius: ");
-                int circleRadius = sc.nextInt();
-                Circle circle = new Circle(shapePosition, circleRadius);
-                shapeCalculations(circle);
-                break;
-            case 2:
-                writeLine("Option 2 selected.");
-                writeLine("Please enter the side length: ");
-                int sideLength = sc.nextInt();
-                Square square = new Square(shapePosition, sideLength);
-                shapeCalculations(square);
-                break;
-            case 3:
-                writeLine("Option 3 selected.");
-                writeLine("Please enter the height: ");
-                int height = sc.nextInt();
-                writeLine("Please enter the width: ");
-                int width = sc.nextInt();
-                Rectangle retangle = new Rectangle(shapePosition, width, height);
-                shapeCalculations(retangle);
-                break;
-            default:
-                writeLine("No menu item selected so the program will exit.");
-                break;
+    
+        // Prompt for shape selection
+        boolean isValidOption = false;
+        while (!isValidOption) {
+            writeLine("Please select a shape. The options are: ");
+            writeLine("1. Circle: ");
+            writeLine("2. Square: ");
+            writeLine("3. Rectangle: ");
+            int shapeNumber = sc.nextInt();
+    
+            switch (shapeNumber) {
+                case 1:
+                    writeLine("Option 1 selected.");
+                    writeLine("Please enter the radius: ");
+                    int circleRadius = sc.nextInt();
+                    Circle circle = new Circle(shapePosition, circleRadius);
+                    shapeCalculations(circle);
+                    isValidOption = true;
+                    break;
+                case 2:
+                    writeLine("Option 2 selected.");
+                    writeLine("Please enter the side length: ");
+                    int sideLength = sc.nextInt();
+                    Square square = new Square(shapePosition, sideLength);
+                    shapeCalculations(square);
+                    isValidOption = true;
+                    break;
+                case 3:
+                    writeLine("Option 3 selected.");
+                    writeLine("Please enter the length: ");
+                    int height = sc.nextInt();
+                    writeLine("Please enter the width: ");
+                    int width = sc.nextInt();
+                    Rectangle rectangle = new Rectangle(shapePosition, width, height);
+                    shapeCalculations(rectangle);
+                    isValidOption = true;
+                    break;
+                default:
+                    writeLine("Invalid option selected. Please select from the three available options.");
+                    break;
+            }
         }
-
+    
         sc.close();
     }
+    
 }
